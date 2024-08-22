d88a8a4b62038cd23cc59d045f9f76b12f410338
Radzhab2024 <shiraev2002@gmail.com>
2024-08-23 01:38:21 +0300

    working version

---
+++ module-3/module_3_1.py
@@ -0,0 +1,23 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+def string_info(string):
+    list_ = [len(string), string.upper(), string.lower()]
+    print(tuple(list_))
+    count_calls()
+def is_contains(string, list_to_search):
+    string = str(string.upper())
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].upper()
+    if string == list_to_search[i]:
+        print(True)
+    else:
+        print(False)
+    count_calls()
+string_info('Radzhab')
+string_info('Complicated')
+is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
+is_contains('cycle', ['universal', 'pictures'])
+print(calls)
